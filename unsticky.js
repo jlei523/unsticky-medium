ad2378bab60ffb27726a606345f17b834e3ee616
jlei523 <jlei523@gmail.com>
2017-10-30 13:57:39 +0800

    Update unsticky.js

--- unsticky.js
+++ unsticky.js
@@ -1,4 +1,4 @@
-let stickiedElements = document.getElementsByClassName("u-fixed");
+var stickiedElements = document.getElementsByClassName("u-fixed");
 
 for (var i = 0; i < stickiedElements.length; i++) {
   stickiedElements[i].classList.remove("u-fixed");
